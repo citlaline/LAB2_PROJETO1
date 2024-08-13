34b7502ccf061a8ca5db15171c4d339fb7161d81
Alinne Citlali Gutiérrez Rohs <B435637@fgv.edu.br>
2024-08-13 10:00:54 -0300

    third commit

--- PRO1.py
+++ PRO1.py
@@ -3,3 +3,13 @@ def cumprimento(texto):
 
 nome_completo = "Alinne Citlali Gutiérrez Rohs"
 cumprimento(nome_completo)
+
+import random
+
+def sorteio_media():
+    numeros = [random.randint(1, 10**6) for _ in range(3)]
+    media = sum(numeros) / len(numeros)
+    return numeros, media
+numeros, media = sorteio_media()
+
+f"Média aritmética: {media}"
